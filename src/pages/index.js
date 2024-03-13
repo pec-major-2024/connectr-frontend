43adca4dc7371bd63bc06f4ec7a11667bec22663
daksh.gupta@gokwik.co <daksh.gupta@gokwik.co>
2024-03-13 20:53:11 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export { Diary } from './Diary';
+export { Home } from './Home';
+export { Login } from './Login';
+export { Register } from './Register';
+export { Statistics, NoteStat } from './Statistics'
+export { Profile } from './Profile';
+export { Recommendation } from './Recommendation'
+export { Favorite } from './Favorite'
+export { Protected } from './Protected'
