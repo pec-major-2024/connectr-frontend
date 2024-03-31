ac8f7d64019dde8f0a03fd0d0add91291432fa96
daksh.gupta@gokwik.co <daksh.gupta@gokwik.co>
2024-03-31 18:08:33 +0530

    heading changes

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -116,8 +116,8 @@ export default function Profile() {
       <LoadingScreen />
       <div className="avatarContainer">
         <div className="backdropAvatar">
-          <h1>S</h1> <h1>E</h1> <h1>N</h1> <h1>T</h1> <h1>I</h1> <h1>M</h1>
-          <h1>O</h1>
+          <h1>C</h1> <h1>O</h1> <h1>N</h1> <h1>N</h1> <h1>E</h1> <h1>C</h1>
+          <h1>T</h1> <h1>r</h1>
         </div>
         <Button
           variant="contained"
